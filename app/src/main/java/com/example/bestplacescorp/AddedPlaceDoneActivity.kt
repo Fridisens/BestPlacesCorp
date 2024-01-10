9def0b02f31feb95a547b25b8941230e9e737b7e
Fridisens <fridisens@gmail.com>
2024-01-10 22:50:53 +0100

    small button updates

--- app/src/main/java/com/example/bestplacescorp/AddedPlaceDoneActivity.kt
+++ app/src/main/java/com/example/bestplacescorp/AddedPlaceDoneActivity.kt
@@ -9,16 +9,34 @@ class AddedPlaceDoneActivity : AppCompatActivity() {
 
 
     lateinit var addMoreButton : Button
+    lateinit var listOfPlacesButton: Button
+    lateinit var goBackSignedInButton: Button
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_added_place_done)
 
-        addMoreButton = findViewById<Button>(R.id.addMoreButton)
 
+        addMoreButton = findViewById<Button>(R.id.addMoreButton)
+        listOfPlacesButton = findViewById(R.id.listOfPlacesButton)
+        goBackSignedInButton = findViewById(R.id.goBackSignedInButton)
 
         addMoreButton.setOnClickListener {
             val intent = Intent(this, RateNameActivity::class.java)
             startActivity(intent)
         }
+
+        listOfPlacesButton.setOnClickListener {
+            val intent = Intent(this, ListOfPlacesRecyclerView::class.java)
+            startActivity(intent)
+        }
+
+        goBackSignedInButton.setOnClickListener {
+            val intent = Intent(this, AddPlaceActivity::class.java)
+            startActivity(intent)
+        }
+
+
     }
 }
